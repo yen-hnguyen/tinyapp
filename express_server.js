2dff768ba43d651c097fce56b3c26da59ca8ef5a
Yen <yen.hnguyen17@gmail.com>
2021-11-27 21:03:40 +0000

    Add EJS and HTML to the template

--- express_server.js
+++ express_server.js
@@ -26,9 +26,14 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
 });
 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
