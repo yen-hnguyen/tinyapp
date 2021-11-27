455006f39e69c7803678d5f8c6fd8f51d63a5084
Yen <yen.hnguyen17@gmail.com>
2021-11-27 22:50:13 +0000

    Fix longURL

--- express_server.js
+++ express_server.js
@@ -32,7 +32,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.shortURL};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
